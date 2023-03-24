b5a65fabfcb3301df9bcf022feb5c1a46b659631
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-24 20:22:08 -0300

    exercicio pronto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string nome;
+Console.WriteLine("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+Console.WriteLine($"{nome}, Digite seu segundo nome: ");
+string sobrenome;
+sobrenome = Console.ReadLine()!;
+string nomecompleto;
+nomecompleto = $"{nome} {sobrenome}";
+Console.WriteLine($"Nome completo: {nomecompleto }");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+
+
+
